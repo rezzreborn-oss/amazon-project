6106f293cb6c6b6332a43be78bcf289e8071f71b
Reza <rezzreborn@gmail.com>
2026-04-10 21:36:31 +0700

    save data and generatae HTML for payment summary

--- project/scripts/utils/money.js
+++ project/scripts/utils/money.js
@@ -1,6 +1,6 @@
 //named export
 export function formatCurrency(priceCents) {
-  return (priceCents / 100).toFixed(2);
+  return (Math.round(priceCents) / 100).toFixed(2);
 }
 
 //dalam 1 file js hanya boleh ada satu default export
