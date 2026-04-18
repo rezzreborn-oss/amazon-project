04e06b16c5fead3f55128d7ba832723a711a02d8
Reza <rezzreborn@gmail.com>
2026-04-18 20:41:20 +0700

    Load products from backend

--- project/scripts/checkout.js
+++ project/scripts/checkout.js
@@ -1,8 +1,11 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {renderCheckoutHeader} from './checkout/checkoutHeader.js';
-import '../data/backend-practice.js'
+import {loadProducts} from '../data/products.js';
+//import '../data/backend-practice.js'
 
-renderCheckoutHeader();
-renderOrderSummary();
-renderPaymentSummary();
+loadProducts(() => {
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+});
